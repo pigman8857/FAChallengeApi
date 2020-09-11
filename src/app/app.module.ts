ed35e79744bd63041195e4b70b41716f510981db
OhSva <somkamolv@hotmail.com>
2020-09-11 15:20:43 +0700

    role back

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,18 +2,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
-import { PaymentDetails } from './payment-details.js/payment-details.js.component';
 import { PaymentDetailsComponent } from './payment-details/payment-details.component';
-import { PaymentDetailComponent } from './payment-details/payment-detail/payment-detail.component';
-import { PaymentDetailListComponent } from './payment-details/payment-detail/payment-detail-list/payment-detail-list.component';
+
 
 @NgModule({
   declarations: [
     AppComponent,
-    PaymentDetails.JsComponent,
-    PaymentDetailsComponent,
-    PaymentDetailComponent,
-    PaymentDetailListComponent
+    PaymentDetailsComponent
   ],
   imports: [
     BrowserModule
